bb1ea095c0d05a33b8f37e784aaf0faeae558459
DMeenakshiReddy <dmeenakshireddy@gmail.com>
2025-11-05 17:12:14 +0530

    Update signup API endpoint to production URL

--- srinu/src/pages/signup.jsx
+++ srinu/src/pages/signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/signup", {
+      const res = await fetch("https://srinu-project.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
