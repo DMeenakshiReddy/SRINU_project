813fb5c190e350e88590e81ed1a117e8e77d78d7
DMeenakshiReddy <dmeenakshireddy@gmail.com>
2025-11-05 17:11:52 +0530

    Update login API endpoint to production URL

--- srinu/src/pages/login.jsx
+++ srinu/src/pages/login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://srinu-project.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
